2dac181ffeba7d0a3f55f627b7b9d8f3cd9fb610
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-02 19:25:19 +0100

    Day 02 - Small refactoring.

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,10 +3,8 @@ fun main() {
     fun isStable(report: List<Int>): Boolean {
         val increasing = report[0] < report[1];
 
-        report.onEachIndexed() { index, _ ->
-            if (index == 0) return@onEachIndexed // skip the first element
-
-            val delta = report[index] - report[index - 1]
+        for (i in 0..<report.lastIndex) {
+            val delta = report[i + 1] - report[i]
 
             if (increasing && (delta < 1 || delta > 3)) return false
 
