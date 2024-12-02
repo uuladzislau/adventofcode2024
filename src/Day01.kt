bf11bd4704ca0cff4ae18e657f3586031ebc6223
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-02 11:13:23 +0100

    Refactor code to avoid collisions in public namespace.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,34 @@
 import kotlin.math.abs
 
 fun main() {
+
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+        // Read input, convert to numeric format, and split into two lists.
+        return input
+            .map { it.split("   ") }
+            .map { it[0].toInt() to it[1].toInt() }
+            .unzip()
+    }
+
+    fun part1(input: List<String>): Int {
+        val (leftInput, rightInput) = parseInput(input)
+
+        val leftSorted = leftInput.sorted().toIntArray()
+        val rightSorted = rightInput.sorted().toIntArray()
+
+        return leftSorted
+            .zip(rightSorted) { left, right -> abs(left - right) }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val (leftInput, rightInput) = parseInput(input)
+
+        val heatmap = rightInput.groupingBy { it }.eachCount()
+
+        return leftInput.sumOf { it * heatmap.getOrDefault(it, 0) }
+    }
+
     val input = readInput("Day01")
 
     check(part1(input) == 1603498)
@@ -8,29 +36,3 @@ fun main() {
     check(part2(input) == 25574739)
 }
 
-fun part1(input: List<String>): Int {
-    val (leftInput, rightInput) = parseInput(input)
-
-    val leftSorted = leftInput.sorted().toIntArray()
-    val rightSorted = rightInput.sorted().toIntArray()
-
-    return leftSorted
-        .zip(rightSorted) { left, right -> abs(left - right) }
-        .sum()
-}
-
-fun part2(input: List<String>): Int {
-    val (leftInput, rightInput) = parseInput(input)
-
-    val heatmap = rightInput.groupingBy { it }.eachCount()
-
-    return leftInput.sumOf { it * heatmap.getOrDefault(it, 0) }
-}
-
-fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
-    // Read input, convert to numeric format, and split into two lists.
-    return input
-        .map { it.split("   ") }
-        .map { it[0].toInt() to it[1].toInt() }
-        .unzip()
-}
