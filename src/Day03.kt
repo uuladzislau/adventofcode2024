639bd4614bb00b72e968eee3075fcc087cbf9759
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-03 18:32:12 +0100

    Day 03 - Part 2 solved.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,24 +1,74 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
-
-        return input.sumOf { line ->
-            regex.findAll(line)
-                .map { matchResult -> matchResult.value }
-                .map { matchedValue ->
-                    // At this point we have a string like "mul(123,456)"
-                    // We drop first 4 characters, then drop last character, then split by comma
-                    // So we get: "mul(123,456)" -> "123,456)" -> "123,456" -> ["123", "456"]
-                    // And then we map each numeric element to integer
-                    val (a, b) = matchedValue.drop(4).dropLast(1).split(",").map { it.toInt() }
-                    a * b
-                }
-                .sum()
-        }
-    }
-
     val input = readInput("Day03")
 
     check(part1(input) == 159833790)
 
+    check(part2(input) == 89349241)
+}
+
+private fun part1(input: List<String>): Int {
+    val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+
+    return input.sumOf { line ->
+        regex.findAll(line)
+            .map { matchResult -> matchResult.value }
+            .map { matchedValue ->
+                // At this point we have a string like "mul(123,456)"
+                // We drop first 4 characters, then drop last character, then split by comma
+                // So we get: "mul(123,456)" -> "123,456)" -> "123,456" -> ["123", "456"]
+                // And then we map each numeric element to integer
+                val (a, b) = matchedValue.drop(4).dropLast(1).split(",").map { it.toInt() }
+                a * b
+            }
+            .sum()
+    }
+}
+
+private interface Operation
+
+private class Do : Operation
+
+private class Dont : Operation
+
+private data class Mul(val a: Int, val b: Int) : Operation {
+    companion object {
+        fun of(s: String): Mul {
+            val (a, b) = s.drop(4).dropLast(1).split(",").map { it.toInt() }
+            return Mul(a, b)
+        }
+    }
+}
+
+private fun part2(input: List<String>): Int {
+
+    val regex = Regex("(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\))")
+
+    val mapped: List<List<Operation>> = input.map { line ->
+        regex.findAll(line)
+            .map { matchResult -> matchResult.value }
+            .map {
+                when {
+                    it.startsWith("mul") -> Mul.of(it)
+                    it == "do()" -> Do()
+                    it == "don't()" -> Dont()
+                    else -> throw IllegalArgumentException("I don't know what to do with $it")
+                }
+            }
+            .toList()
+    }
+
+    var shouldDo = true;
+    var sum = 0;
+
+    for (line in mapped) {
+        for (operation in line) {
+            when (operation) {
+                is Mul -> if (shouldDo) sum += operation.a * operation.b
+                is Do -> shouldDo = true
+                is Dont -> shouldDo = false
+            }
+        }
+    }
+
+    return sum
 }
