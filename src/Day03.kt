da0e47fc4eef4ad194fbbe34983a3c48acbcb176
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-03 19:19:53 +0100

    Day 03 - Big refactoring.

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,67 +7,41 @@ fun main() {
 }
 
 private fun part1(input: List<String>): Int {
-    val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+    val regex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
 
     return input.sumOf { line ->
         regex.findAll(line)
             .map { matchResult -> matchResult.value }
             .map { matchedValue ->
-                // At this point we have a string like "mul(123,456)"
-                // We drop first 4 characters, then drop last character, then split by comma
-                // So we get: "mul(123,456)" -> "123,456)" -> "123,456" -> ["123", "456"]
-                // And then we map each numeric element to integer
-                val (a, b) = matchedValue.drop(4).dropLast(1).split(",").map { it.toInt() }
+                val (a, b) =
+                    matchedValue
+                        .removeSurrounding("mul(", ")")
+                        .split(",")
+                        .map { it.toInt() }
                 a * b
             }
             .sum()
     }
 }
 
-private fun part2(input: List<String>): Int {
+private fun part2(lines: List<String>): Int {
+    val fullMemory = lines.joinToString("")
 
-    val regex = Regex("(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\))")
+    val regex = Regex("""(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))""")
 
-    val mapped: List<List<Operation>> = input.map { line ->
-        regex.findAll(line)
-            .map { matchResult -> matchResult.value }
-            .map {
-                when {
-                    it.startsWith("mul") -> Mul.of(it)
-                    it == "do()" -> Do()
-                    it == "don't()" -> Dont()
-                    else -> throw IllegalArgumentException("I don't know what to do with $it")
-                }
-            }
-            .toList()
-    }
+    var shouldMultiply = true
+    var sum = 0
 
-    var shouldMultiply = true;
-
-    return mapped.sumOf { line ->
-        var sum = 0;
-        for (operation in line) {
-            when (operation) {
-                is Mul -> if (shouldMultiply) sum += operation.a * operation.b
-                is Do -> shouldMultiply = true
-                is Dont -> shouldMultiply = false
+    regex.findAll(fullMemory).forEach { match ->
+        when (match.value) {
+            "do()" -> shouldMultiply = true
+            "don't()" -> shouldMultiply = false
+            else -> if (shouldMultiply) {
+                val (a, b) = match.value.removeSurrounding("mul(", ")").split(",")
+                sum += a.toInt() * b.toInt()
             }
         }
-        sum
     }
+
+    return sum
 }
-
-private class Do : Operation
-
-private class Dont : Operation
-
-private data class Mul(val a: Int, val b: Int) : Operation {
-    companion object {
-        fun of(s: String): Mul {
-            val (a, b) = s.drop(4).dropLast(1).split(",").map { it.toInt() }
-            return Mul(a, b)
-        }
-    }
-}
-
-private interface Operation
