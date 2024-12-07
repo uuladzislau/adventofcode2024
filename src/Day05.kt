733c817bb4fc4ff0e2543c209275151455e4a820
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-07 21:16:13 +0100

    Day 05 - Part 2 solved.

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,12 +8,50 @@ fun main() {
     val updates = rawUpdates.lines().map { it.split(",") }
 
     check(part1(rules, updates) == 5129)
+    check(part2(rules, updates) == 4077)
 }
 
-fun part1(rules: Map<String, List<String>>, updates: List<List<String>>): Int {
+private fun part1(rules: Map<String, List<String>>, updates: List<List<String>>): Int {
     return updates
         .filter { it.allInCorrectOrder(rules) }
         .sumOf { it[it.size / 2].toInt() }
+}
+
+private fun part2(rules: Map<String, List<String>>, updates: List<List<String>>): Int {
+    return updates
+        .filter { !it.allInCorrectOrder(rules) }
+        .map {
+            var mutable = it
+            do {
+                mutable = mutable.order(rules)
+            } while (!mutable.allInCorrectOrder(rules))
+            mutable
+        }
+        .sumOf { it[it.size / 2].toInt() }
+}
+
+private fun List<String>.order(rules: Map<String, List<String>>): List<String> {
+    val positions = this.mapIndexed { i, item -> item to i }.toMap()
+
+    for (rule in rules) {
+        if (positions[rule.key] == null) continue
+
+        for (nextPage in rule.value) {
+            if (positions[nextPage] == null) continue
+
+            val pagePosition = positions.getValue(rule.key)
+            val nextPagePosition = positions.getValue(nextPage)
+
+            if (pagePosition > nextPagePosition) {
+                val reordered = this.toMutableList()
+                reordered[nextPagePosition] = rule.key
+                reordered[pagePosition] = nextPage
+                return reordered.toList()
+            }
+        }
+    }
+
+    return this
 }
 
 private fun List<String>.allInCorrectOrder(rules: Map<String, List<String>>): Boolean {
