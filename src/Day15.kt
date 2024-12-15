8d0a06d7571e6f6860292794f591974969496001
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-15 18:38:36 +0100

    Day 15 - Preparations for part 2.

--- src/Day15.kt
+++ src/Day15.kt
@@ -3,10 +3,13 @@ import kotlin.io.path.readText
 
 fun main() {
     val testInput = parseInput("Day15_test")
+    val testInputPart2 = parseInput("Day15_test", true)
     val input = parseInput("Day15")
 
     check(part1(testInput) == 10092)
     check(part1(input) == 1465523)
+
+    part2(testInputPart2)
 }
 
 private fun part1(warehouse: Warehouse): Int {
@@ -15,15 +18,43 @@ private fun part1(warehouse: Warehouse): Int {
     return warehouse.map.filterValues { it == 'O' }.keys.sumOf { it.first * 100 + it.second }
 }
 
-private fun parseInput(fileName: String): Warehouse {
+private fun part2(warehouse: Warehouse): Int {
+    warehouse.print()
+    warehouse.letRobotMove()
+
+    return 0
+}
+
+private fun parseInput(fileName: String, widthDoubled: Boolean = false): Warehouse {
     val (rawWarehouseMap, rawVelocities) = Path("src/$fileName.txt").readText().trim().split("\n\n")
 
-    val map: WarehouseMap = buildMap {
-        rawWarehouseMap.lines()
-            .flatMapIndexed { i, line ->
-                line.mapIndexed { j, el -> put(i to j, el) }
+    val map: WarehouseMap = mutableMapOf()
+
+    rawWarehouseMap.lines().forEachIndexed { i, line ->
+        line.forEachIndexed { j, el ->
+            if (!widthDoubled) {
+                map[i to j] = el
+            } else {
+                val doubledJ = j * 2
+
+                if (el == '@') {
+                    map[i to doubledJ] = '@'
+                    map[i to doubledJ + 1] = '.'
+                } else if (el == 'O') {
+                    map[i to doubledJ] = '['
+                    map[i to doubledJ + 1] = ']'
+                } else if (el == '#') {
+                    map[i to doubledJ] = '#'
+                    map[i to doubledJ + 1] = '#'
+                } else {
+                    map[i to doubledJ] = '.'
+                    map[i to doubledJ + 1] = '.'
+                }
+
             }
-    }.toMutableMap()
+
+        }
+    }
 
     val velocities = rawVelocities.lines()
         .flatMap { line ->
@@ -93,6 +124,15 @@ private data class Warehouse(val map: WarehouseMap, val directions: List<Coordin
 
         // box is still there, can't move :(
     }
+
+    fun print() = print(buildString {
+        (0..9).forEach { i ->
+            (0..19).forEach { j ->
+                append(map[i to j] ?: ' ')
+            }
+            append('\n')
+        }
+    })
 }
 
 private typealias WarehouseMap = MutableMap<Coordinate, Char>
