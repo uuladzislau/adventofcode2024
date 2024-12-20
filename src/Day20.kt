91450f963a90cf06ace633caa5bf38277484d1d3
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-20 13:14:06 +0100

    Day 20 - Part 1 in progress.

--- src/Day20.kt
+++ src/Day20.kt
@@ -9,22 +9,17 @@ private fun part1(map: Grid): Int {
     val start = map.find('S')
     val end = map.find('E')
 
-    val (score, path) = findPath(map, start, end, 0)
-
-    path.println()
-    score.println()
-
-    printPath(map, path)
+    findPath(map, start, end).println()
 
     return 0
 }
 
-private fun findPath(map: Grid, start: Coordinate, end: Coordinate, cheatsLeft: Int): Pair<Int, Set<Triple<Coordinate, Direction, Int>>> {
+private fun findPath(map: Grid, start: Coordinate, end: Coordinate): Int {
     data class State(
         val from: Coordinate,
         val direction: Direction,
         val score: Int,
-        val path: Set<Triple<Coordinate, Direction, Int>>
+        val cheated: Boolean
     )
 
     val visiting = mutableListOf<State>()
@@ -32,15 +27,13 @@ private fun findPath(map: Grid, start: Coordinate, end: Coordinate, cheatsLeft: 
     val visited = mutableSetOf<Pair<Coordinate, Direction>>() // storing pairs of location + direction
 
     visiting.add(
-        State(start, Direction.LEFT, 0, emptySet())
+        State(start, Direction.LEFT, 0, false)
     )
 
     while (visiting.isNotEmpty()) {
-        visiting.sortBy { it.score } // always prefer nodes with lower score, like in Dijkstra's
+        val (loc, direction, score) = visiting.removeFirst()
 
-        val (loc, direction, score, path) = visiting.removeFirst()
-
-        if (loc == end) return score to path
+        if (loc == end) return score
 
         if (loc to direction in visited) continue
 
@@ -48,38 +41,13 @@ private fun findPath(map: Grid, start: Coordinate, end: Coordinate, cheatsLeft: 
 
         val nextLoc = loc + direction.offset
 
-        val currentPath = path + Triple(loc, direction, score)
-
         if (map[nextLoc] != '#') {
-            visiting += State(nextLoc, direction, score + 1, currentPath)
+            visiting += State(nextLoc, direction, score + 1, false)
         }
 
-        visiting += State(loc, direction.plus90(), score, path)
-        visiting += State(loc, direction.minus90(), score, path)
+        visiting += State(loc, direction.plus90(), score, false)
+        visiting += State(loc, direction.minus90(), score, false)
     }
 
     error("Can't solve :(")
-}
-
-private fun printPath(map: Grid, path: Set<Triple<Coordinate, Direction, Int>>) {
-    val pathMapping = path.map { it.first to it.second }.toMap()
-    buildString {
-        for (i in map.indices) {
-            for (j in map[0].indices) {
-                if (pathMapping[i to j] != null) {
-                    append(
-                        when (pathMapping.getValue(i to j)) {
-                            Direction.UP -> '^'
-                            Direction.DOWN -> 'v'
-                            Direction.RIGHT -> '>'
-                            Direction.LEFT -> '<'
-                        }
-                    )
-                } else {
-                    append(map[i to j])
-                }
-            }
-            append('\n')
-        }
-    }.println()
 }
