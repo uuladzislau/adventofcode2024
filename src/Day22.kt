56132065ea355f65b4d52cc03c0139762e03a280
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-22 18:40:05 +0100

    Day 22 - Part 2 in progress.

--- src/Day22.kt
+++ src/Day22.kt
@@ -8,7 +8,8 @@ fun main() {
     check(part1(testInput) == 37327623L)
     check(part1(input) == 17005483322)
 
-    part2(listOf(123))
+    part2(listOf(1, 2, 3, 2024))
+//    part2(input) // 2077 is too high
 }
 
 private fun part1(secretNumbers: List<Int>): Long = secretNumbers.sumOf {
@@ -19,18 +20,36 @@ private fun part1(secretNumbers: List<Int>): Long = secretNumbers.sumOf {
 }
 
 private fun part2(secretNumbers: List<Int>) {
-
-    val priceChanges = secretNumbers.map { secretNumber ->
-        val nextSecretNumbers = sequenceOf(secretNumber) + generateSequence(secretNumber) { nextSecretNumber(it) }
+    val pricePerCustomer = secretNumbers.map { secretNumber ->
+        (sequenceOf(secretNumber) + generateSequence(secretNumber) { nextSecretNumber(it) })
             .drop(1)
-            .take(9) // todo: take" 1999
-
-        secretNumber to nextSecretNumbers.map { it % 10 }.zipWithNext { a, b -> b - a }.toList()
+            .take(1999) // todo: take" 1999
+            .map { it % 10 }
+            .toList()
     }
 
-    priceChanges.println()
+    val patterns = mutableMapOf<String, Int>()
 
+    for (prices in pricePerCustomer) {
+        for (i in 4..prices.lastIndex) {
+            val price1 = prices[i - 3] - prices[i - 4]
+            val price2 = prices[i - 2] - prices[i - 3]
+            val price3 = prices[i - 1] - prices[i - 2]
+            val price4 = prices[i - 0] - prices[i - 1]
 
+            val pattern = "$price1,$price2,$price3,$price4"
+//
+//            if (patterns.contains(pattern)) {
+//                println("Windows: '$pattern', current price: ${prices[i]}")
+//            }
+
+            val hits = patterns.getOrDefault(pattern, 0)
+
+            patterns[pattern] = hits + prices[i]
+        }
+    }
+
+    patterns.values.max().println()
 }
 
 private fun nextSecretNumber(initialSecretNumber: Int): Int = initialSecretNumber
