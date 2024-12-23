04b6e97be99feda8b5977f112db27233cc6fbdb7
Uladzislau Ulasovich <uladzislau.ulasovich@icloud.com>
2024-12-23 11:57:31 +0100

    Day 23 - Refactoring.

--- src/Day23.kt
+++ src/Day23.kt
@@ -9,19 +9,17 @@ fun main() {
 private fun part1(input: List<String>): Int {
 
     val connections = buildMap<String, MutableSet<String>> {
-        input.map { it.split("-") }.map { it[0] to it[1] }.forEach { (comp1, comp2) ->
-            computeIfAbsent(comp1) { mutableSetOf() }.add(comp2)
-            computeIfAbsent(comp2) { mutableSetOf() }.add(comp1)
+        input.map { it.split("-") }.map { it[0] to it[1] }.forEach { (pc1, pc2) ->
+            computeIfAbsent(pc1) { mutableSetOf() }.add(pc2)
+            computeIfAbsent(pc2) { mutableSetOf() }.add(pc1)
         }
     }
 
     val triples = buildSet {
         for ((pc1, connectedTo) in connections) {
             for (pc2 in connectedTo) {
-                val pc2Connections = connections.getValue(pc2).filter { it != pc1 }
-
-                for (pc3 in pc2Connections) {
-                    if (connections.getValue(pc3).filter { it != pc3 }.contains(pc1)) add(setOf(pc1, pc2, pc3))
+                for (pc3 in connections.getValue(pc2)) {
+                    if (connections.getValue(pc3).contains(pc1)) add(setOf(pc1, pc2, pc3))
                 }
             }
         }
